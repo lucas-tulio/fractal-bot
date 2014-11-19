6420f208176fecdd52ae0a2ad4a8170804331742
Tulio <tulio@doubleleft.com>
2014-11-19 19:52:57 -0200

    Using twitter stream api to read mentions, sending fractals

--- manager.py
+++ manager.py
@@ -1,4 +1,35 @@
 import tweepy
+import time
+import os
+import sys
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+import json
+
+latestId = 0
+
+def sendFractal(latestId, tweet):
+  print "got one"
+  if latestId == 0 or latestId != tweet["id"]:
+  
+    print "this is a new one! Generating fractal..."
+    tweetText = tweet["text"]
+    os.system("python mandelbrot.py " + tweetText[13:])
+
+    print "Sending tweet"
+    latestId = tweet["id"]
+    tweetUser = tweet["user"]
+    api.update_with_media("mandelbrot.png", "@" + tweetUser["screen_name"] + " Here's your fractal", in_reply_to_status_id=latestId)
+    print "done!"
+
+class listener(StreamListener):
+  def on_data(self, data):
+    jsonData = json.loads(data)
+    sendFractal(latestId, jsonData)
+    return True
+  def on_error(self, status):
+    print "Error!"
+    sys.exit()
 
 # Read parameters. They should be in the order shown below and
 # follow the format: parameter=value
@@ -12,11 +43,9 @@ f.close()
 # Authenticate
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
-
 api = tweepy.API(auth)
+stream = tweepy.Stream(auth, listener())
 
-# Read tweets
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-  print tweet.id
-
+# Start reading mention stream
+print "reading mentions..."
+stream.filter(track=["@fractal_bot"])
