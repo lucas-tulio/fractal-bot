29d73a8e02c4a2d819df6c1fd38b92e5dc4d3595
Tulio <tulio@doubleleft.com>
2014-11-19 18:42:07 -0200

    Basic twitter api access

---
+++ manager.py
@@ -0,0 +1,21 @@
+import tweepy
+
+# Read parameters
+f = open("config.txt", "r")
+consumer_key = f.readline().split("=")[1].rstrip("\n")
+consumer_secret = f.readline().split("=")[1].rstrip("\n")
+access_token = f.readline().split("=")[1].rstrip("\n")
+access_token_secret = f.readline().split("=")[1].rstrip("\n")
+f.close()
+
+# Authenticate
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+# Read tweets
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+  print tweet.id
+
