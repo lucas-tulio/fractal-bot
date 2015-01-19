36206b3e0dd48e10be8fbdf6caaf9355bb6c0cb7
Tulio <tulio@doubleleft.com>
2015-01-19 16:48:06 -0200

    Added fractal of the day log table

--- db.sql
+++ db.sql
@@ -1,4 +1,5 @@
 create schema fractal;
+use fractal;
 
 -- Use this table to prevent sending fractals to the same person too often
 create table logs (
@@ -11,5 +12,14 @@ create table logs (
 create table blacklist (
   id int primary key auto_increment,
   username varchar(255) not null,
+  created_at timestamp default current_timestamp
+);
+
+-- Log the fractal of the day
+create table fotd (
+  id int primary key auto_increment,
+  link varchar(255) not null,
+  deletehash varchar(255) not null,
+  size int not null,
   created_at timestamp default current_timestamp
 );
