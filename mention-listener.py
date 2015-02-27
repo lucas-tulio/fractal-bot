f1482b4e10216f76582d5e33a8f15a5a59a64163
Tulio <tulio@doubleleft.com>
2015-02-27 18:40:04 -0300

    Better log system

--- mention-listener.py
+++ mention-listener.py
@@ -15,16 +15,19 @@ sentences = [
   "I made this fractal just for you"
 ]
 
+def log(text):
+  print str(datetime.now()) + ": " + text
+
 def send_fractal(tweet):
 
-  print str(datetime.now()) + ": got one"
+  log("got one")
 
   # Don't do anything if it's our own tweet
   tweet_id = tweet["id"]
   tweet_user = tweet["user"]
   username = tweet_user["screen_name"]
   if username == "fractal_bot":
-    print "not sending one to myself"
+    log("not sending one to myself")
     return
 
   # Check if it's a mention
@@ -33,43 +36,43 @@ def send_fractal(tweet):
   is_mention = False
   for mention in user_mentions:
     if mention["screen_name"] == "fractal_bot":
-      print "mentions " + str(mention["screen_name"])
+      log("mentions " + str(mention["screen_name"]))
       is_mention = True
-      print "mention!"
+      log("mention!")
       break
   if not is_mention:
-    print "not a mention"
+    log("not a mention")
     return
 
   # Skip if it's a retweet
   if "retweeted_status" in tweet:
-    print "retweet, skipping"
+    log("retweet, skipping")
     return
-  print "not a retweet"
+  log("not a retweet")
 
   # Check the already sent list
   if not db.can_send(username):
-    print "already sent to this guy " + str(username)
+    log("already sent to this guy " + str(username))
     return
   else:
     db.save_send(username)
 
-  print "log to this user saved: " + str(username)
+  log("log to this user saved: " + str(username))
 
   # Check the blacklist
   if db.is_user_in_blacklist(username):
-    print "blacklist. Skipping user " + str(username)
+    log("blacklist. Skipping user " + str(username))
     return
 
-  print "not in blacklist"
+  log("not in blacklist")
 
-  print "generating fractal to " + str(username)
+  log("generating fractal to " + str(username))
   fractal.generate()
 
-  print "Sending tweet"
+  log("Sending tweet")
   sentence = random.choice(sentences)
   twitter.api.update_with_media("fractal.png", "@" + username + " " + sentence, in_reply_to_status_id=tweet_id)
-  print "done!"
+  log("done!")
 
 class Listener(StreamListener):
   def on_data(self, data):
@@ -77,22 +80,22 @@ class Listener(StreamListener):
     send_fractal(json_data)
     return True
   def on_error(self, status):
-    print "Error!"
-    print status
+    log("Error!")
+    log(status)
     db._disconnect()
     sys.exit()
 
 # Start Fractal generator
 fractal = Fractal()
 
+# Start database
+db = Database()
+
 # Start Twitter
 twitter = Twitter()
 stream = Stream(twitter.auth, Listener())
 
-# Start database
-db = Database()
-
 # Start reading stream
-print "reading mentions"
+log("reading mentions")
 stream.filter(track=["fractal_bot"])
-print str(datetime.now()) + "script stopped running"
+log("script stopped running")
